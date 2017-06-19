5876e4390077db4e0eb0247a062547ed1c202fe9
reza mirzaei <reza.mirzaeifard@gmail.com>
2017-06-19 17:19:35 +0430

    :(:(

--- src/main/java/ir/golestan/dao/TermDAO.java
+++ src/main/java/ir/golestan/dao/TermDAO.java
@@ -1,7 +1,21 @@
 package ir.golestan.dao;
 
+import ir.golestan.model.Term;
+
+import javax.persistence.Entity;
+
 /**
  * Created by Reza-PC on 6/19/2017.
  */
+@Entity
 public class TermDAO {
+    public void create(){
+
+    }
+    public void delete(){
+
+    }
+    public Term load(){
+        return null;
+    }
 }
