fa207ac9369fe226ab13836e372d9a694d47b376
reza mirzaei <reza.mirzaeifard@gmail.com>
2017-06-21 15:05:23 +0430

    make TermController

---
+++ src/main/java/ir/golestan/controller/TermController.java
@@ -0,0 +1,39 @@
+package ir.golestan.controller;
+
+import ir.golestan.model.Term;
+import ir.golestan.service.TermService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * Created by Reza-PC on 6/21/2017.
+ */
+@Controller
+public class TermController {
+    @Autowired
+    HttpSession httpSession;
+    @Autowired
+    TermService termService;
+    @RequestMapping(value = "/maketerm",method = RequestMethod.GET)
+    public String mainTerm(Model model){
+        if(httpSession.getAttribute("username")!=null&&((String)httpSession.getAttribute("rule")).compareTo("admin")==0){
+            List<Term> terms = termService.loadAll();
+            model.addAllAttributes(terms);
+            return "makeTerm";
+        }
+        return "home";
+    }
+    @RequestMapping(value = "/maketerm" ,method = RequestMethod.POST)
+    public String makeTerm(@RequestParam("name")String name,@RequestParam("code")Long code,@RequestParam("year")int year,@RequestParam("season")int season){
+       return null;
+    }
+
+}
